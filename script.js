1073a86196c8fb41564b0d9fd2d0b2605353eb8e
Josh King <162310653+jjjmdev@users.noreply.github.com>
2024-06-24 00:47:07 +0800

    Display books

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const container = document.querySelector(".container");
 
 function Book(title, author, pages, read) {
 	this.title = title;
@@ -9,11 +10,24 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(book) {
 	myLibrary.push(book);
+}
 
-	console.log(myLibrary);
-	// Update HTML
+function updateCards() {
+	container.innerHTML = myLibrary.reduce(
+		(text, book) =>
+			text +
+			`<div class="card">
+				<div class="title">Title: ${book.title}</div>
+				<div class="author">Author: ${book.author}</div>
+				<div class="pages">Pages: ${book.pages}</div>
+				<div class="read">Read: ${book.read}</div>
+			</div>`,
+		""
+	);
 }
 
 addBookToLibrary(new Book("Redwine Supernova", "Chapell Roan", 28, true));
 addBookToLibrary(new Book("American Teenager", "Ethel Cain", 96, false));
 addBookToLibrary(new Book("Mequetrefe", "Arca", 666, false));
+
+updateCards();
